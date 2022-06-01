caf132bb8d7cdfd00fc00b65580a8dff1e8fcd0a
Lydia <lydia.haway@gmail.com>
2022-06-01 21:17:47 +0200

    ex

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,8 +1,8 @@
-let important = document.querySelectorAll(".important");
+let important = document.querySelectorAll(".important"); // querySelectorALl -- to select every element not just the first one
 
 important.forEach(function () {
   important.title = "This is an important item";
-}); // ForEach with the querySelectorAll!
+}); // ForEach with the querySelectorAll! --- change the title for all element with the class important
 
 console.log(important.title);
 
@@ -10,7 +10,7 @@ let images = document.querySelectorAll("img");
 
 images.forEach(function (i) {
   if (i.classList != "important") {
-    i.style.display = "none";
+    i.style.display = "none"; // we only change the element without the class important
   }
 });
 
