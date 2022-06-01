8c23383a5e6e133dd14e0ed239be09d6cd095af4
Lydia <lydia.haway@gmail.com>
2022-06-01 16:06:46 +0200

    ex

---
+++ 1.Introduction/script.js
@@ -0,0 +1,15 @@
+console.log(document.title); // Display the title
+
+document.title = "Modifying the DOM"; // change the title
+
+console.log(document.title);
+
+//document.body.style.backgroundColor = "#FF69B4"; // change the background color
+
+const RandomColor = Math.floor(Math.random() * 16777215).toString(16); // Get a random color for the background
+
+document.body.style.backgroundColor = "#" + RandomColor;
+
+const bodyChildren = document.body.children;
+
+console.log(bodyChildren);
