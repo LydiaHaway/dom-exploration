9f1debdcaa5b80486772e7c0422f4bf3e37095eb
Lydia <lydia.haway@gmail.com>
2022-06-02 16:06:03 +0200

    ex

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,2 +1,30 @@
-/*
-*/
+const ol = document.querySelector("ol");
+
+const a = ol.firstElementChild;
+const b = ol.lastElementChild;
+
+console.log(a);
+console.log(b);
+
+ol.removeChild(b);
+ol.insertBefore(b, a);
+
+let main = document.querySelector("main");
+
+let section = document.querySelector("section");
+
+let ThirdSection = section.nextElementSibling;
+
+let titleThirdSection = ThirdSection.firstElementChild;
+
+let divSecondSection = ThirdSection.nextElementSibling;
+
+let SecondSection = divSecondSection.firstElementChild;
+
+console.log(SecondSection);
+
+let titleSecondSection = SecondSection.firstElementChild;
+
+SecondSection.appendChild(ThirdSection);
+
+SecondSection.removeChild(ThirdSection);
